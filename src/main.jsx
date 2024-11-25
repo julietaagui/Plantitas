b41173589f7e8cc61145009cb7f5c174f1711d03
julietaagui <aguilerajulieta700@gmail.com>
2024-11-25 16:54:49 -0300

    se agregan cambios

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,8 @@
-// main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css'; // Importación de Bootstrap
-import './app.css'; // Asegúrate de que index.css esté en la carpeta 'styles'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'; 
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(

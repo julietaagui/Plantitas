d64da419d0f3dcd13e6d02257347c84ec9685b02
julietaagui <aguilerajulieta700@gmail.com>
2025-03-26 11:01:59 -0300

    se agregan cambios

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css'; 
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>,
-);
+  </StrictMode>,
+)

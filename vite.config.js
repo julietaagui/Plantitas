fb4f19f57ff7624b2635971be6a69d348d472ee7
julietaagui <aguilerajulieta700@gmail.com>
2025-03-26 11:28:27 -0300

    se agregan cambios

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Esto ayuda a que los archivos se sirvan bien en Netlify
+  base: './', // Importante para Netlify
 });
